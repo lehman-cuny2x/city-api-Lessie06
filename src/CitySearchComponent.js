53994db7eb3663551ddcfd3ebab4866579060eb4
Lessie Quezada <Lessieq07@gmail.com>
2021-06-11 11:24:17 -0400

    Fixed this file and added the map method here to call all the elements in the api data

--- src/CitySearchComponent.js
+++ src/CitySearchComponent.js
@@ -4,18 +4,19 @@ import React from 'react';
 class CitySearchComponent extends React.Component{
     constructor(props){
         super(props);
-        this.state={
-            Zips:this.props.Zips
-        }
+        
         
     }
 
 
     render(){
 
+        let items=[];
+        items = this.props.zips.map((i)=> {return <p>Zip Codes in that area {i}</p>})
+
         return(
             <div>
-                <p>Zip Codes in that area {this.state.Zips}</p>
+                {items}
             </div>
         )
     }
